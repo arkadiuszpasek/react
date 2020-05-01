aa2dc86a15ed681f134adfabc3b5a6915ce4e22a
Arkadiusz Pasek <thevik444@gmail.com>
2020-05-01 09:23:14 +0200

    Updates

---
+++ precache-manifest.23053352adcf80741680b64c85fd640c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82bb5a076a348b832355d66fb48b314c",
+    "url": "/Modern-React-with-Redux-2020-UdemyCourse/index.html"
+  },
+  {
+    "revision": "526fdb4c94d27bc4a343",
+    "url": "/Modern-React-with-Redux-2020-UdemyCourse/static/css/main.60394f57.chunk.css"
+  },
+  {
+    "revision": "0422d7777fd1062b056f",
+    "url": "/Modern-React-with-Redux-2020-UdemyCourse/static/js/2.3fb01d8a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/Modern-React-with-Redux-2020-UdemyCourse/static/js/2.3fb01d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526fdb4c94d27bc4a343",
+    "url": "/Modern-React-with-Redux-2020-UdemyCourse/static/js/main.49a9e29e.chunk.js"
+  },
+  {
+    "revision": "97964432213ccf0f1831",
+    "url": "/Modern-React-with-Redux-2020-UdemyCourse/static/js/runtime-main.1080f5e5.js"
+  }
+]);
